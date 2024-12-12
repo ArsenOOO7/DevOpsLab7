df61150a893d0f9ee06d6ff45a10d1be5ce75386
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2024-12-12 20:41:50 +0200

    Init commit

---
+++ src/main/java/com/arsen/devops/minidatadog/MiniDataDogApplication.java
@@ -0,0 +1,13 @@
+package com.arsen.devops.minidatadog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDataDogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDataDogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arsen/devops/minidatadog/MiniDataDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsen.devops.minidatadog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDataDogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
